ac92a286cb1ff009cb90923140fb5354aa995466
maeda <maeda@illi-ichi.com>
2013-12-11 02:15:44 +0900

    forの中でパターンマッチも使えるという例

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -18,19 +18,13 @@ object Main {
       } else None
     }
 
-    def generate_complex(u1: User, u2: User): Option[String] = {
-      if(affinity.contains((u1, u2)) && affinity.contains((u2, u1))){
-        val emotion1 = affinity((u1, u2))
-        val emotion2 = affinity((u2, u1))
-        if(emotion1 == emotion2) {
-          if(message.contains((u1, inverse(emotion1)))){
-            Some(template(u1, u2, message((u1, inverse(emotion1)))))
-          }else if(message.contains((u1, emotion1))){
-            Some(template(u1, u2, message((u1, emotion1))))
-          }else None
-        } else None
-      } else None
-    }
+    def generate_complex(u1: User, u2: User): Option[String] = 
+      for {
+        emotion1 <- affinity.get((u1, u2))
+        `emotion1` <- affinity.get((u2, u1))
+        msg <- message.get((u1, inverse(emotion1)))
+               .orElse (message.get((u1, emotion1)))
+      } yield template(u1, u2, msg)
 
     private def template(u1: User, u2: User, msg: String) = s"$u2「$msg？」\n$u1「$msg！」"
 
